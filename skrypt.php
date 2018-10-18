f2423313d535b4f247097012adb8c8d71244b642
Sirek Piotr <32463186+sirekpiotr@users.noreply.github.com>
2018-10-18 14:01:41 +0200

    juz jest niezle ;)

--- skrypt.php
+++ skrypt.php
@@ -2,19 +2,18 @@
 
     $connection = mysqli_connect('localhost', 'root', '');
     mysqli_select_db($connection, 'muzea');
-
-    $zapytanie = "SELECT * FROM mytable limit 2";
+    $zapytanie = "SELECT * FROM mytable WHERE Miejscowosc='Warszawa'";
     $q = mysqli_query($connection,$zapytanie);
     while($row = mysqli_fetch_assoc($q))
     {
-        $request = "http://open.mapquestapi.com/geocoding/v1/address?key=uFDfwtV8W4SU9JgvL6offFufMPjNYxkV&location=".$row['Miejscowosc'].",".$row['Ulica'].",".$row['Numer_domu'];
+        $request = "http://open.mapquestapi.com/geocoding/v1/address?key=uFDfwtV8W4SU9JgvL6offFufMPjNYxkV&location=".$row['Kod_pocztowy'].",".$row['Miejscowosc'].",".$row['Ulica'].",".$row['Numer_domu'];
         $json = file_get_contents($request);
         $obj = json_decode($json,true);
-        //echo $obj->results->0->locations->0->latLng->lat;
-        //echo $obj->results->0->locations->0->latLng->lng;
         $lat = $obj['results']['0']['locations']['0']['latLng']['lat'];
         $lng = $obj['results']['0']['locations']['0']['latLng']['lng'];
-        
+        $update = "UPDATE mytable SET x=$lng, y=$lat WHERE Lp=".$row['Lp'];
+        mysqli_query($connection,$update);
+        echo $update."<br>";
     }
     //requestAPI -> http://open.mapquestapi.com/geocoding/v1/address?key=uFDfwtV8W4SU9JgvL6offFufMPjNYxkV&location=Opole,Minorytow,3
     //keyAPi -> uFDfwtV8W4SU9JgvL6offFufMPjNYxkV
